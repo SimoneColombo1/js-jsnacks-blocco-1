cc3cf4bfd73073ddff6eb7b8f6c84fa1edf08107
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-13 12:36:05 +0200

    esercizio 5

---
+++ snack 5/js/script.js
@@ -0,0 +1,20 @@
+
+
+for(i=0; i<7;i++){
+    let NumUtente= prompt('inserisci un numero');
+    let Num = [
+  
+    ];
+    
+    if(NumUtente % 2 == 0)
+        {
+
+
+        }
+        else{
+           Num.push(NumUtente);
+        }
+
+        console.log(Num);
+}
+
