034804efd7be585f1cac53c696e7813e6aa7aa3d
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-13 11:52:53 +0200

    creata la variabile per far inserire i numeri all'utente e l'if else per calcolare il maggiore

---
+++ snack 1/JS/script.js
@@ -0,0 +1,16 @@
+const numeroA=  
+        prompt("numeroA");
+    const numeroB=   prompt("numeroB"); 
+
+
+
+   
+
+ 
+ if (numeroA > numeroB) {
+    console.log(numeroA ,'è maggiore');
+    
+ }
+ else{
+    console.log(numeroB ,'è maggiore');
+ }
