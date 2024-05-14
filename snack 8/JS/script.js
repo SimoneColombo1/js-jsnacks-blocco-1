c5a1d0bbcad6c2a246a5d2006cab53415381f9d7
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-14 12:06:10 +0200

    .

--- snack 8/JS/script.js
+++ snack 8/JS/script.js
@@ -1,28 +1,7 @@
 const body= document.getElementById(' article #container');
 const DivRosso= document.createElement('div');
 const DivVerde= document.createElement('div');
-const Numeri= [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-]
+const Numeri= [1, 2, 3, 4, 5, 6, 7,8,9,10, 11,12, 13,14,15,16,17,18,19,20]; 
 
 container.appendChild(DivVerde);
 container.appendChild(DivRosso);
@@ -33,3 +12,27 @@ DivVerde.setAttribute("class", DivVerde.getAttribute("rosso") );
 
 DivRosso.className = 'rosso';
 DivVerde.className = 'verde';
+
+for( let i=0; i< Numeri.length; i++){
+const span= document.createElement('p');
+span.append(Numeri[i]);
+if(Numeri[i] % 2==0){
+    DivVerde.appendChild(span);
+
+}
+else{
+    DivRosso.appendChild(span);
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
